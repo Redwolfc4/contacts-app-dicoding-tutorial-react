c9d067c76d2aceab3f3ac9c90118ad0a6260e8f5
Salahudin Kholik Prasetyono <Salahudinkoliq10@gmail.com>
2025-09-22 08:46:07 +0700

    first commit

---
+++ src/components/ContactItemBody.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ContactItemBody({ name, tag }) {
+  return (
+    <div className="contact-item__body">
+      <h2 className="contact-item__title">{name}</h2>
+      <p className="contact-item__username">@{tag}</p>
+    </div>
+  );
+}
+
+export default ContactItemBody;
