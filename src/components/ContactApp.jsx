95fa317fbf8091e2730c692322bb64fa1a7331e8
Salahudin Kholik Prasetyono <Salahudinkoliq10@gmail.com>
2025-09-26 21:43:55 +0700

    Add ContactInput component for adding new contacts and update ContactApp to integrate it

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ContactList from "./ContactList";
 import { getData } from "../utils/data";
+import ContactInput from "./ContactInput";
 
 class ContactApp extends React.Component {
   constructor(props) {
@@ -11,6 +12,8 @@ class ContactApp extends React.Component {
 
     // wajib kita bind
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    // add contact
+    this.addContactHandler = this.onAddContactHandler.bind(this);
   }
 
   onDeleteHandler(id) {
@@ -18,10 +21,32 @@ class ContactApp extends React.Component {
       contacts: prevState.contacts.filter((contact) => contact.id !== id),
     }));
   }
+
+  onAddContactHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imageUrl: "/images/default.jpeg",
+          },
+        ],
+      };
+    });
+  }
+
   render() {
     return (
       <div className="contact-app">
-        <h1>Daftar Kontak</h1>
+        <h1>Aplikasi Kontak</h1>
+
+        <h2>Tambah Kontak</h2>
+        <ContactInput addContactHandler={this.addContactHandler} />
+
+        <h2>Daftar Kontak</h2>
         <ContactList
           contacts={this.state.contacts}
           onDelete={this.onDeleteHandler}
---
+++ src/components/ContactInput.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+class ContactInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      tag: "",
+    };
+
+    this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+    this.onChangeNameHandler = this.onChangeNameHandler.bind(this);
+    this.onChangeTagHandler = this.onChangeTagHandler.bind(this);
+  }
+
+  onSubmitEventHandler(e) {
+    e.preventDefault();
+
+    this.props.addContactHandler(this.state);
+  }
+
+  onChangeNameHandler() {
+    this.setState({ name: event.target.value });
+  }
+
+  onChangeTagHandler() {
+    this.setState({ tag: event.target.value });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitEventHandler} className="contact-input">
+        <input
+          type="text"
+          placeholder="Nama"
+          value={this.state.name}
+          onChange={this.onChangeNameHandler}
+        />
+        <input
+          type="text"
+          placeholder="Tag"
+          value={this.state.tag}
+          onChange={this.onChangeTagHandler}
+        />
+        <button type="submit">Tambah Kontak</button>
+      </form>
+    );
+  }
+}
+
+export default ContactInput;
